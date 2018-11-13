5f9eedc2064a036e9cf579b2565f187b4b1ff350
phitranpt <phi.tran.pt@gmail.com>
2018-11-13 12:01:51 -0600

    finished question 4. will finish the rest after lunch

--- joins-solution.sql
+++ joins-solution.sql
@@ -2,11 +2,29 @@
 
 -- Get all customers and their addresses.
 
+-- SELECT customers.first_name, addresses.street
+-- FROM customers
+-- JOIN addresses ON addresses.customer_id = customers.id;
+
 -- Get all orders and their line items (orders, quantity and product).
+
+-- SELECT orders.id, line_items.quantity, products.description FROM orders
+-- JOIN line_items ON line_items.order_id = orders.address_id
+-- JOIN products ON products.id = line_items.product_id;
 
 -- Which warehouses have cheetos?
 
+-- SELECT warehouse.warehouse, products.description FROM warehouse
+-- JOIN warehouse_product ON warehouse_product.warehouse_id = warehouse.id
+-- JOIN products ON products.id = warehouse_product.product_id
+-- WHERE products.description = 'cheetos';
+
 -- Which warehouses have diet pepsi?
+
+-- SELECT warehouse.warehouse, products.description FROM warehouse
+-- JOIN warehouse_product ON warehouse_product.warehouse_id = warehouse.id
+-- JOIN products ON products.id = warehouse_product.product_id
+-- WHERE products.description = 'diet pepsi';
 
 -- Get the number of orders for each customer. NOTE: It is OK if those without orders are not included in results.
 
